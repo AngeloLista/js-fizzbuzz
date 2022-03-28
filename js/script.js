3c58a710b98eca7ffb8f6b802b4dcbc6471a8679
AngeloLista <abstractplayer@gmail.com>
2022-03-28 18:25:49 +0200

    FizzBuzz result printed on console

--- js/script.js
+++ js/script.js
@@ -5,7 +5,10 @@
 let result = "";
 
 for (let i = 1; i <= 100; i++) {
-    result += i + ' ';
+    if (i % 3 === 0 && i % 5 === 0) result += 'FizzBuzz - ';
+    else if (i % 3 === 0) result += 'Fizz - ';
+    else if (i % 5 === 0) result += 'Buzz - ';
+    else result += i + ' - ';
 }
 
-console.log('numeri da 1 a 100: ', result)
+console.log(result)
